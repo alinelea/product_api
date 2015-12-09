a3dabb28020dd0b433214489ae950700f4828b44
Aline <alineleaadams@gmail.com>
2015-12-09 08:52:00 -0600

    updated routes

--- product_api/config/routes.rb
+++ product_api/config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  namespace :api do
-  namespace :v1 do
-    get 'products/index'
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :products, only: [:index, :show]
     end
   end
 
-  namespace :api do
-  namespace :v1 do
-    get 'products/show'
-    end
-  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
